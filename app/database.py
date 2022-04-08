c990a4756dc67948aa791ec05bb6feeac0754cc4
Szymon Kownacki <szymon.kownacki@gmail.com>
2022-04-08 11:28:57 +0200

    Fix DB connection in heroku

--- app/database.py
+++ app/database.py
@@ -3,9 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:maze123@localhost:6432/maze')
+db_uri = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:maze123@localhost:6432/maze')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql+psycopg2://", 1)
+
+engine = create_engine(db_uri)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
